c893b689272697519000deac22ae296a6d1afc6c
zengf <zengf@certusnet.com.cn>
2019-04-19 18:47:54 +0800

    update

--- src/basic/BreakContinue.java
+++ src/basic/BreakContinue.java
@@ -17,5 +17,16 @@ public class BreakContinue {
         // continue也可以带标签
 
         // break与continue是可选的，可以完全不用，一样能表达相同的语义
+
+        // 多层break continue
+        for (int i = 0; i < 3; i++) {
+            loop: for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    System.out.println(String.format("i, j, k = %s, %s, %s", i, j, k));
+//                    break loop;
+                    continue loop;
+                }
+            }
+        }
     }
 }
